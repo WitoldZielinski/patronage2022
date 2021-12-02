ba0bbad1e800a6faceb87e2bb9542b1378d5fa84
Witold zielinski <witar6@gmail.com>
2021-12-02 12:52:13 +0100

    JS methods for html form upgrade

--- login.js
+++ login.js
@@ -2,42 +2,38 @@ const btn = document.getElementById("btn-login");
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
-let userFlag = false; 
-let passworFlag = false;
+
 
 
 function checkPassword () {
     for (let i = 0; i < sessionStorage.length; i++) {
         const user = sessionStorage.key(i);
         if (JSON.parse(sessionStorage.getItem(user)).password === password.value) {
-            passworFlag = true;
-            password.setCustomValidity('');
-            password.reportValidity();
+            return true;
         } else {
-            passworFlag = false;
-            password.setCustomValidity('Nieprawidłowa nazwa użytkownika i/lub hasło');
-            password.reportValidity();
+            return false;
         }
     }
 }
 
 function checkUser () {  
-    if (sessionStorage.getItem('' + username.value) === null) {
-        userFlag = false;
+    if (sessionStorage.getItem(username.value) === null) {
+        return false;
     } else {
-        userFlag = true;
+        return true;
     }
 }
 
 function logIn(e) {
     e.preventDefault();
-    userFlag = false;
-    passworFlag = false;
-    checkUser();
-    checkPassword();
-
-    if (userFlag && passworFlag) {
-        window.location = "logged.html";
+    if (checkUser() && checkPassword()) {
+        password.setCustomValidity('');
+        password.reportValidity();
+        window.location.assign("logged.html");
+    } else {
+        password.setCustomValidity('Nieprawidłowa nazwa użytkownika i/lub hasło');
+        password.reportValidity();
+        return false;
     }
 }
 
